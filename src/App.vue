03d08a4f845a6278e62c726cab741fb3dc89a73f
Resacas <73529170+Resacas@users.noreply.github.com>
2021-05-03 13:35:18 +0200

    piezas del robot y canvas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <h1>hola</h1>
+    </header>
+    <main>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+      </div>
+      <router-view/>
+    </main>
+    <footer>
+      <h1>adios</h1>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,95 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <figure class="tamaño_S">
+      <img src="@/assets/robot/cabeza.svg" alt="" id="cabeza_S">
+    </figure>
+    <figure class="tamaño_M">
+      <img src="@/assets/robot/cabeza.svg" alt="" class="active" id="cabeza_M">
+    </figure>
+    <figure class="tamaño_L">
+      <img src="@/assets/robot/cabeza.svg" alt="" id="cabeza_L">
+    </figure>
+
+
+    <figure class="tamaño_M">
+      <img src="@/assets/robot/boca.svg" alt="" class="active" id="cabeza_M">
+    </figure>
+    <figure class="tamaño_M">
+      <img src="@/assets/robot/nariz.svg" alt="" class="active" id="cabeza_M">
+    </figure>
+    <figure class="tamaño_M">
+      <img src="@/assets/robot/ojos.svg" alt="" class="active" id="cabeza_M">
+    </figure>
+    
+    <canvas id="crear_robot"></canvas>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
+
+window.onload = function() {
+  let dpi = window.devicePixelRatio;
+  //get canvas
+  let canvas = document.getElementById('myCanvas');
+  //get context
+  let ctx = canvas.getContext('2d');
+  function fix_dpi() {
+    //get CSS height
+    //the + prefix casts it to an integer
+    //the slice method gets rid of "px"
+    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+    //get CSS width
+    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+    //scale the canvas
+    canvas.setAttribute('height', style_height * dpi);
+    canvas.setAttribute('width', style_width * dpi);
+  }
+  
+  var robot = document.getElementsByClassName("active");
+  ctx.drawImage(robot[0], 50, 10, 70, 40);
+  ctx.drawImage(robot[1], 76, 38, 20, 3);
+  ctx.drawImage(robot[2], 82, 31, 8, 5);
+  ctx.drawImage(robot[3], 71, 30, 30, 2);
+
+  console.log(robot[0], robot[1], robot[2], robot[3])
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  figure{
+    padding:0.5vw;
+    cursor:pointer;
+  }
+
+  .active{
+    border: 3px solid purple;
+    border-radius: 50%;
+  }
+
+  .tamaño_S{
+    width: 2.5vw;
+    height: 2.5vw;
+  }
+  .tamaño_M{
+    width:3vw;
+    height:3vw;
+  }
+  .tamaño_L{
+    width: 3.5vw;
+    height: 3.5vw;
+  }
+
+
+
+  canvas{
+    width: 50vw;
+    height: 70vh;
+    border: 6px solid orange;
+    display:block;
+  }
 </style>
